9ca824dba55d388a6911d12cc7925843521a83e6
Graphene101102 <84337302+Graphene101102@users.noreply.github.com>
2024-05-11 00:52:53 +0700

    them 1 phan UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="viet_app">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <div className="board_columns">
+      <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.pinimg.com/564x/1e/92/b3/1e92b3f2edf7064da7c4ba7d4998f26a.jpg" alt="Viet-alt-img"/>
+            Title: QUANG VIET
+            </li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.pinimg.com/564x/1e/92/b3/1e92b3f2edf7064da7c4ba7d4998f26a.jpg" alt="Viet-alt-img"/>
+            Title: QUANG VIET
+            </li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.pinimg.com/564x/1e/92/b3/1e92b3f2edf7064da7c4ba7d4998f26a.jpg" alt="Viet-alt-img"/>
+            Title: QUANG VIET
+            </li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.pinimg.com/564x/1e/92/b3/1e92b3f2edf7064da7c4ba7d4998f26a.jpg" alt="Viet-alt-img"/>
+            Title: QUANG VIET
+            </li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+            <li>Hoang Nguyen Quang Viet</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
