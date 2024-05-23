7719944331e443129d1432a02f293dc04062c75c
Graphene101102 <84337302+Graphene101102@users.noreply.github.com>
2024-05-23 21:03:35 +0700

    drag-drop day 1

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react"
+import { Container, Draggable } from "react-smooth-dnd";
 import { isEmpty } from "lodash";
 import './BoardContent.scss'
 import Column from "../Column/Column";
@@ -13,7 +14,6 @@ function BoardContent() {
             const boardFromDB = initialData.board.find(board => board.id === 'board-1')
             if (boardFromDB) {
                 setBoard(boardFromDB)
-
                 //sort column
                 setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
             }
@@ -22,9 +22,31 @@ function BoardContent() {
         if(isEmpty(board)) {
             return <div className="not-found" style={{'padding': '10px', 'color': 'white'}}>Board not found</div>
         }
+
+        const onColumnDrop = (dropResult) =>{
+            console.log(dropResult)
+        }
     return (
         <div className="board_conten">
-             {columns.map((column, index) => <Column key={index} column={column} />)}  
+         <Container
+          orientation="horizontal"
+          onDrop={onColumnDrop}
+          getChildPayload={index => columns[index]}
+          dragHandleSelector=".column-drag-handle"
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'column-drop-preview'
+          }}
+        >
+            {columns.map((column, index) => (
+                <Draggable key={index}>
+                    <Column column={column} />
+                </Draggable>
+            
+            ))}  
+        </Container>
+             
             
         </div>
     )
