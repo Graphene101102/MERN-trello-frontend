d51a7c6d91a04878b7d782e02f8cf54059350d05
Graphene101102 <84337302+Graphene101102@users.noreply.github.com>
2024-05-25 17:09:01 +0700

    add-new-col
    succes

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
+import { Container as BoostrapContainer, Row, Col, Form, Button } from 'react-bootstrap';
 import { Container, Draggable } from "react-smooth-dnd";
 import { isEmpty } from "lodash";
 import './BoardContent.scss'
@@ -10,6 +11,10 @@ import { applyDrag } from "utilities/dragDrop";
 function BoardContent() {
     const [board, setBoard] = useState({});
     const [columns, setColumns] = useState([]);
+    const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+    const [newColumnTitle, setNewColumnTitle] = useState('')
+
+    const onNewColumnTitleChange = useCallback((e) => setNewColumnTitle(e.target.value), [])
 
     useEffect(() => {
         const boardFromDB = initialData.board.find(board => board.id === 'board-1')
@@ -52,6 +57,30 @@ function BoardContent() {
         }
     }
 
+    const addNewColumn = () => {   
+        const newColumnToAdd = {
+            id:Math.random().toString(36).substring(2,5),
+            boardId: board.id,
+            title: newColumnTitle.trim(),
+            cardOrder: [],
+            cards: []
+        }
+        let newColumns = [...columns]
+        newColumns.push(newColumnToAdd)
+
+        let newBoard = { ...board }
+        newBoard.columnOrder = newColumns.map(c => c.id)
+        newBoard.columns = newColumns
+
+        setColumns(newColumns)
+        setBoard(newBoard)
+
+        setNewColumnTitle('')
+        toggleOpenNewColumn()
+
+    }
+
+    const toggleOpenNewColumn = () => setOpenNewColumnForm(!openNewColumnForm)
     return (
         <div className="board_conten">
             <Container
@@ -72,9 +101,31 @@ function BoardContent() {
 
                 ))}
             </Container>
-            <div className="add-new-column">
-                <i className="fa fa-plus icon" /> Add another column
-            </div>
+
+            <BoostrapContainer className="container">
+                {!openNewColumnForm && <Row>
+                    <Col className="add-new-column" onClick={toggleOpenNewColumn}>
+                        <i className="fa fa-plus icon" /> Add another column
+                    </Col>
+                </Row>}
+
+                {openNewColumnForm && <Row>
+                    <Col className="enter-new-column">
+                        <Form.Control
+                            size="sm"
+                            type="text"
+                            placeholder="Enter column title..."
+                            className="input-new-column"
+                            value={newColumnTitle}
+                            onChange={onNewColumnTitleChange}
+                            onKeyDown={event => (event.key === 'Enter') && addNewColumn()}
+                        />
+                        <Button variant="success" size="sm" onClick={addNewColumn}>Add column</Button>{' '}
+                        <span className="cancel-new-column" onClick={toggleOpenNewColumn}> <i className="fa fa-trash icon" /></span>
+                    </Col>
+                </Row>}
+
+            </BoostrapContainer>
 
         </div>
     )
